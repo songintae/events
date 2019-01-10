af34e2725377d0a450e84765d7565426e05da56b
intaesong <intaesong@woowahan.com>
2019-01-10 23:22:16 +0900

    Study용 프로젝트 초기 설정

---
+++ events-api/src/main/java/events/EventsApplication.java
@@ -0,0 +1,11 @@
+package events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventsApplication.class, args);
+    }
+}
