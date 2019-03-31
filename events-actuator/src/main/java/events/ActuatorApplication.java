ab2b1e41fd81ea271e0a8de5f1fc30c4bae949f0
intaesong <intaesong@woowahan.com>
2019-03-31 14:08:11 +0900

    스프링 actuator 적용 및 admin client 등록

---
+++ events-actuator/src/main/java/events/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package events;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+}
